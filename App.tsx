d2bafaefb78883a8ffc59d6795d68c50cf551d0c
PicoShot <116891491+PicoShot@users.noreply.github.com>
2024-08-31 21:45:50 +0300

    base

--- App.tsx
+++ App.tsx
@@ -1,21 +1,40 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, {useState} from 'react';
+
 
 export default function App() {
+
+  const [discordUser, SetDiscordUser] = useState('')
+  const [discordId, setDiscordId] = useState()
+  const [state, setState] = useState()
+
   return (
     <View style={styles.container}>
-      <Text> open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Discord Username</Text>
+      <TextInput style={styles.TextInputStyle} placeholder='Enter Discord Username'/>
+
+      <Text>Discord ID</Text>
+      <TextInput keyboardType='numeric' style={styles.TextInputStyle} placeholder='Enter Discord ID'/>
+
+      
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    textAlign:'center',
+  container:{
+    flex:1,
+    backgroundColor:'#fff',
+    alignItems:'center',
+    justifyContent:'center',
   },
-});
+  TextInputStyle:{
+    borderBottomWidth:1,
+    width:'80%',
+    height:50,
+    borderRadius:20,
+    marginVertical:10,
+    textAlign:'center',
+    
+  }
+})
